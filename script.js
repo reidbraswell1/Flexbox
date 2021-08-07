5f3f5f0e2d96e79df7b1b67cd9ba407de2c702fa
Reid Braswell <reidbras@gmail.com>
2021-08-07 17:53:15 -0500

    Correcting logic errors in script call

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-document.getElementById("flex-row-direction").addEventListener('click', flexRow);
-document.getElementById("flex-column-direction").addEventListener('click', flexColumn);
-
+document
+  .getElementById("flex-row-direction")
+  .addEventListener("click", flexRow);
+document
+  .getElementById("flex-column-direction")
+  .addEventListener("click", flexColumn);
+document
+  .getElementById("row-justify-start")
+  .addEventListener("click", function () {
+    flexJustify("start");
+  });
+document
+  .getElementById("row-justify-end")
+  .addEventListener("click", function () {
+    flexJustify("end");
+  });
 
 function flexRow() {
-    console.log("--Entering Flex Row");
-    let row = document.getElementById("flexbox-row");
-    row.style.flexDirection="row";
+  console.log("--Entering Flex Row--");
+  let row = document.getElementById("flexbox-row");
+  row.style.flexDirection = "row";
 }
 
 function flexColumn() {
-    console.log("--Entering Flex Column");
-    let column = document.getElementById("flexbox-row");
-    column.style.flexDirection="column";
+  console.log("--Entering Flex Column--");
+  let column = document.getElementById("flexbox-row");
+  column.style.flexDirection = "column";
+}
+
+function flexJustify(startEnd) {
+  console.log(`--Entering Flex Justify-- ${startEnd}`);
+  let justify = document.getElementById("flexbox-row");
+  justify.style.justifyContent = startEnd;
 }
