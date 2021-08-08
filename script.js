a5085537c1b09f7995f544d9912bc21db4f5550d
reid <reidbras@gmail.com>
2021-08-07 22:30:30 -0500

    Adding script for center on align-items and justify-content

--- script.js
+++ script.js
@@ -3,20 +3,47 @@ document
   .addEventListener("click", function () {
     flexDirection("row");
   });
+
 document
   .getElementById("flex-column-direction")
   .addEventListener("click", function () {
     flexDirection("column");
   });
+
 document
   .getElementById("row-justify-start")
   .addEventListener("click", function () {
     flexJustify("start");
   });
+
+document
+  .getElementById("row-justify-center")
+  .addEventListener("click", function () {
+    flexJustify("center");
+  });
+
 document
   .getElementById("row-justify-end")
   .addEventListener("click", function () {
     flexJustify("end");
+  });
+
+document
+  .getElementById("row-align-start")
+  .addEventListener("click", function () {
+     flexAlign("start");
+  });
+
+document
+  .getElementById("row-align-center")
+  .addEventListener("click", function() {
+    flexAlign("center");
+  });
+
+document
+  .getElementById("row-align-end")
+  .addEventListener("click", function () {
+     flexAlign("end");
   });
 
 function flexDirection(rowColumn) {
@@ -29,4 +56,10 @@ function flexJustify(startEnd) {
   console.log(`--Entering Flex Justify-- ${startEnd}`);
   let justify = document.getElementById("flexbox-row");
   justify.style.justifyContent = startEnd;
+}
+
+function flexAlign(startEnd) {
+  console.log(`--Entering Flex Align-- ${startEnd}`);
+  let align = document.getElementById("flexbox-row");
+  align.style.alignItems = startEnd;
 }
