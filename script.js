e3ff94a098f67390717b481dd99dedab62c04a36
Reid Braswell <reidbras@gmail.com>
2021-08-09 14:53:21 -0500

    Adding try/catch blocks to script

--- script.js
+++ script.js
@@ -1,59 +1,111 @@
-let test = document.getElementById("flex-direction");
-document
-  .getElementById("flex-direction")
-  .addEventListener("change", function () {
-    let index = this.selectedIndex;
-    if (this[index].value != "Choose") {
-      flexDirection(this[index].value);
-    }
-  });
-
-document
-  .getElementById("flex-justify-content")
-  .addEventListener("change", function () {
-    let index = this.selectedIndex;
-    if (this[index].value != "Choose") {
-      flexJustify(this[index].value);
-    }
-  });
-
-document
-  .getElementById("flex-align-items")
-  .addEventListener("change", function () {
-    let index = this.selectedIndex;
-    if (this[index].value != "Choose") {
-      flexAlign(this[index].value);
-    }
-  });
-
+try {
+  console.log(`-- Entering "flex-direction" event listener.`);
   document
-  .getElementById("flex-align-self")
-  .addEventListener("change", function () {
-    let index = this.selectedIndex;
-    if (this[index].value != "Choose") {
-      flexAlignSelf(this[index].value);
-    }
-  });
-
-function flexDirection(rowColumn) {
-  console.log(`--Entering Flex Direction -- ${rowColumn}`);
-  let row = document.getElementById("flexbox-row");
-  row.style.flexDirection = rowColumn;
+    .getElementById("flex-direction")
+    .addEventListener("change", function () {
+      let index = this.selectedIndex;
+      if (this[index].value != "Choose") {
+        flexDirection(this[index].value);
+      }
+    });
+} catch (ex) {
+  console.log(
+    `-- An error occurred getting the "flex-direction" object. Exception ${ex}`
+  );
 }
 
-function flexJustify(startEnd) {
-  console.log(`--Entering Flex Justify-- ${startEnd}`);
-  let justify = document.getElementById("flexbox-row");
-  justify.style.justifyContent = startEnd;
+try {
+  console.log(`-- Entering "flex-justify-content" event listener.`);
+  document
+    .getElementById("flex-justify-content")
+    .addEventListener("change", function () {
+      let index = this.selectedIndex;
+      if (this[index].value != "Choose") {
+        flexJustify(this[index].value);
+      }
+    });
+} catch (ex) {
+  console.log(
+    `-- An error occurred getting the "flex-justify-content" object. Exception ${ex}`
+  );
 }
 
-function flexAlign(startEnd) {
-  console.log(`--Entering Flex Align-- ${startEnd}`);
-  let align = document.getElementById("flexbox-row");
-  align.style.alignItems = startEnd;
+try {
+  console.log(`-- Entering "flex-align-items" event listener.`);
+  document
+    .getElementById("flex-align-items")
+    .addEventListener("change", function () {
+      let index = this.selectedIndex;
+      if (this[index].value != "Choose") {
+        flexAlign(this[index].value);
+      }
+    });
+} catch (ex) {
+  console.log(
+    `-- An error occurred getting the "flexbox-row" object. Exception ${ex}`
+  );
 }
-function flexAlignSelf(startEnd) {
-  console.log(`--Entering Flex Align Self -- ${startEnd}`);
-  let align = document.getElementById("flexbox-row-item-2");
-  align.style.alignSelf = startEnd;
+
+try {
+  console.log(`-- Entering "flex-align-self" event listener.`);
+  document
+    .getElementById("flex-align-self")
+    .addEventListener("change", function () {
+      let index = this.selectedIndex;
+      if (this[index].value != "Choose") {
+        flexAlignSelf(this[index].value);
+      }
+    });
+} catch (ex) {
+  console.log(
+    `-- An error occurred getting the "flex-align-self" object. Exception ${ex}`
+  );
+}
+
+function flexDirection(paramValue) {
+  console.log(`-- Entering Flex Direction -- paramValue=${paramValue}`);
+  try {
+    let obj = document.getElementById("flexbox-row");
+    obj.style.flexDirection = paramValue;
+  } catch (exception) {
+    console.log(
+      `-- An error occurred getting the "flexbox-row" object. Exception ${exception}`
+    );
+  }
+}
+
+function flexJustify(paramValue) {
+  console.log(`-- Entering Flex Justify-- paramValue=${paramValue}`);
+  try {
+    let obj = document.getElementById("flexbox-row");
+    obj.style.justifyContent = paramValue;
+  } catch (exception) {
+    console.log(
+      `-- An error occurred getting the "flexbox-row" object. Exception ${exception}`
+    );
+  }
+}
+
+function flexAlign(paramValue) {
+  console.log(`-- Entering Flex Align-- paramValue=${paramValue}`);
+  try {
+    let obj = document.getElementById("flexbox-row");
+    obj.style.alignItems = paramValue;
+  } catch (exception) {
+    console.log(
+      `-- An error occurred getting the "flexbox-row" object. Exception ${exception}`
+    );
+  }
+}
+
+function flexAlignSelf(paramValue) {
+  console.log(`-- Entering Flex Align Self -- paramValue=${paramValue}`);
+  try {
+    let obj = document.getElementById("flexbox-row-item-2");
+    obj.style.alignSelf = paramValue;
+  } catch (exception) {
+    console.log(
+      `-- An error occurred getting the "flexbox-row-item-2" object. Exception ${exception}`
+    );
+  }
 }
